13b8bd83fade4ac44fec082d38150bba7ad21a84
youarewinner <youarewinner@naver.com>
2013-07-21 22:23:07 +0900

    Create search.py

---
+++ search.py
@@ -0,0 +1,15 @@
+# seach file 찾기 python 2
+# -*- coding: utf-8 -*-
+import urllib2
+import re
+
+text = '드림라인'
+html_content = urllib2.urlopen('http://www.dreamline.kr/dreamline-blog').read()
+matches = re.findall(text ,html_content)
+
+
+if len(matches) == 0:
+    print ' i did not find anything'
+else:
+    print 'My string is in the html'
+    print text
