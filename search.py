18452948d71ee5d4685ba78c5bfebd6fa22a96fa
youarewinner <youarewinner@naver.com>
2024-12-25 23:22:56 +0900

    Update search.py

--- search.py
+++ search.py
@@ -7,7 +7,7 @@ text = '드림라인'
 html_content = urllib2.urlopen('http://www.dreamline.kr/dreamline-blog').read()
 matches = re.findall(text ,html_content)
 
-
+#
 if len(matches) == 0:
     print ' i did not find anything'
 else:
